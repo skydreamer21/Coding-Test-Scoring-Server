e5bc6df89bd0d723495c551bbe79e7432744dfb7
skydreamer21 <skydreamer210@gmail.com>
2023-06-27 15:45:09 +0900

    feat: create ScoreController and add score api

---
+++ src/main/java/com/skydream/scoringserver/controller/ScoringController.java
@@ -0,0 +1,27 @@
+package com.skydream.scoringserver.controller;
+
+import com.skydream.scoringserver.model.SolutionDto;
+import com.skydream.scoringserver.service.IScoringService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/score")
+public class ScoringController {
+    private final IScoringService scoringService;
+
+    public ScoringController(IScoringService scoringService) {
+        this.scoringService = scoringService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<String> score(@RequestBody SolutionDto solutionDto) {
+        System.out.println(solutionDto);
+        String result = scoringService.score(solutionDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
