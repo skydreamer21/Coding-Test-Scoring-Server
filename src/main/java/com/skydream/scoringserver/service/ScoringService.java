a207fdf23bf0a9e17f056ae97d58667a96d7483c
skydreamer21 <skydreamer210@gmail.com>
2023-06-27 17:49:35 +0900

    feat: add saving user code feature

--- src/main/java/com/skydream/scoringserver/service/ScoringService.java
+++ src/main/java/com/skydream/scoringserver/service/ScoringService.java
@@ -2,10 +2,29 @@ package com.skydream.scoringserver.service;
 
 import com.skydream.scoringserver.model.SolutionDto;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 @org.springframework.stereotype.Service
 public class ScoringService implements IScoringService {
+    private static final String USER_CODE_PATH ="C:\\Create\\development\\edu\\ssafy\\secondSem\\ScoringServer\\src\\main\\resources\\usercode\\usercode.java";
+
+
     @Override
     public String score(SolutionDto solutionDto) {
-        return null;
+        saveUserCodeFile(solutionDto);
+        return "SUCCESS";
+    }
+
+    private void saveUserCodeFile(SolutionDto solutionDto) {
+        try {
+            OutputStream output = new FileOutputStream(USER_CODE_PATH);
+            String str = solutionDto.getCode();
+            byte[] by = str.getBytes();
+            output.write(by);
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 }
