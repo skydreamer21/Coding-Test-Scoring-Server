eecf0708c3731156f01d2ec01e5f69276d7d08cf
skydreamer21 <skydreamer210@gmail.com>
2023-06-27 14:52:19 +0900

    chore: create springboot project

---
+++ src/main/java/com/skydream/scoringserver/ScoringServerApplication.java
@@ -0,0 +1,13 @@
+package com.skydream.scoringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skydream/scoringserver/ScoringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skydream.scoringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
